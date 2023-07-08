b590ebaadaf3d3681cd58b85cd2d2e981690b053
Visinho <Elvisvisinho@rocketmail.com>
2023-07-08 23:23:03 +0100

    Connected to the MongoDB database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
     providers: [
@@ -13,9 +14,15 @@ const handler = NextAuth({
     },
     async signIn({ profile }){
         try {
-            
+            await connectToDB();
+            // To check if a user already exists
+
+            // if not, create a new user
+
+            return true;
         } catch (error) {
-            
+            console.log(error);
+            return false;
         }
     }
 })
--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,25 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// let isConnected = false; // for tracking the connection
+let isConnected = false; // for tracking the connection
+
+export const connectToDB = async () => {
+    mongoose.set("strictQuery", true);
+
+    if(isConnected){
+        console.log("MongoDB is connected");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "Visinho_Prompts",
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        isConnected = true;
+        console.log("MongoDB Connected")
+    } catch (error) {
+        console.log(error)
+    }
+}

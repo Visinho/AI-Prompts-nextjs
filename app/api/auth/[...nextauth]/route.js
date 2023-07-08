4630c7945aceb173807521b5071c27d74356b55d
Visinho <Elvisvisinho@rocketmail.com>
2023-07-08 23:38:39 +0100

    Created the model for our user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
 
+import User from "@models/user";
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({

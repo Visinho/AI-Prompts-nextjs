f23b5e7e03cff89d825820171dd6e53e41a63d77
Visinho <Elvisvisinho@rocketmail.com>
2023-07-12 10:53:53 +0100

    Successfully designed the search field component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
-  return (
-    <div>
-      Feed
-    </div>
-  )
-}
 
-export default Feed
+  const [searchText, setsearchText] = useState("")
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+          className="search_input peer"
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required />
+      </form>
+    </section>
+  );
+};
+
+export default Feed;

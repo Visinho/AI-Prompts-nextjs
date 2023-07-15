65a6876200159d26e2c19663cb77f68847300197
Visinho <Elvisvisinho@rocketmail.com>
2023-07-15 22:31:11 +0100

    Fetched the prompt by ID to be edited

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React from "react";
+import Form from "@components/Form";
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const EditPrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+  const [submitting, setsubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+    const response = await fetch(`/api/prompt/${promptId}`)
+        const data = await response.json();
+
+        setPost({
+            prompt: data.prompt,
+            tag: data.tag,
+        })
+}
+if(promptId) getPromptDetails();
+  }, [promptId])
+  
+
+//   const createPrompt = async (e) => {
+//     e.preventDefault();
+//     setsubmitting(true);
+
+//     try {
+//       const response = await fetch("/api/prompt/new", {
+//         method: "POST",
+//         body: JSON.stringify({
+//           prompt: post.prompt,
+//           userId: session?.user.id,
+//           tag: post.tag,
+//         }),
+//       });
+
+//       if (response.ok) {
+//         router.push("/");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     } finally {
+//       setsubmitting(false);
+//     }
+//   };
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={() => {}}
+    />
+  );
+};
+
+export default EditPrompt;
